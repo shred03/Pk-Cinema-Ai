122de9e97290981f01357a47e8383c19db078ee9
shred03 <mithunyada12345@gmail.com>
2025-02-19 12:04:58 +0530

    last_name

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     user_id: { type: Number, required: true, unique: true },
     username: {type: String, default: 'Untitled'},
     first_name: String,
-    last_name: String,
+    last_name: {type: String, default: 'NA'},
     created_at: { type: Date, default: Date.now }
 });
 
