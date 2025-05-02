fdde838434d9dda4fce635662f32f48dd891cfb5
shred03 <mithunyada12345@gmail.com>
2025-05-02 19:49:32 +0530

    fixed known issue

--- server/plugins/setupRoutes.js
+++ server/plugins/setupRoutes.js
@@ -2,17 +2,14 @@ const express = require('express');
 const statusRoutes = require('./statusRoutes');
 
 const setupStatus = (app) => {
-   
-    app.use('/status', statusRoutes);
-    
+
+    app.use('/', statusRoutes);
     app.get('/health', (req, res) => {
         res.json({
             status: 'ok',
             timestamp: new Date().toISOString()
         });
     });
-
-    app.use('/', statusRoutes);
 };
 
 module.exports = setupStatus;
