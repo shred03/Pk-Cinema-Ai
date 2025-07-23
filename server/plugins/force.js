55e038c66b3bbe04d9f6753c23f6816e6afff761
shred03 <mithunyada12345@gmail.com>
2025-07-23 23:29:45 +0530

    new force channel added

--- server/plugins/force.js
+++ server/plugins/force.js
@@ -2,6 +2,7 @@ module.exports = {
     FORCE_CHANNELS: [
       { id: '-1002205905018', username: 'PirecyKings3', name: '𝗣𝗶𝗿𝗲𝗰𝘆 𝗞𝗶𝗻𝗴𝘀 𝟯.𝟬 👑' },
       { id: '-1002301716866', username: 'pirecykingcollectionandfranchise', name: '[PK 👑] Collection/franchise'},
+      {id: '-1001990700007', username: 'Animeke14hai', name: 'Anime Ke 14 ༺ ✧ |'},
      
     ],
 }
