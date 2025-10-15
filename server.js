a9b202fb2c1a8bc7605eb6047ea9e836620dabdc
shred03 <vitsmithun01@gmail.com>
2025-10-16 00:43:41 +0530

    updated code routes

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get("/", async(req, res) => {
     });
 })
 
+app.get("/pirecykings/:uniqueId", async (req, res) =>{
+    try {
+        const {uniqueId} = req.params;
+        const botUsername = process.env.BOT_USERNAME
+        return res.redirect(`https://t.me/${botUsername}?start=${uniqueId}`)
+    } catch (error) {
+        console.error('Redirect error:', error);
+        return res.status(500).send('Server error');
+    }
+})
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Health check server is running on port ${PORT}`);
